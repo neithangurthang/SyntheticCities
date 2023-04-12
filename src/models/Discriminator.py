1330fc7df4f7fea0dd3aa0ef310b5c8ef3b8f31e
Furio Sordini <furiosordini@gmail.com>
2023-04-12 19:13:42 +0000

    update on RL

---
+++ src/models/Discriminator.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+class OptDis(nn.Module):
+    def __init__(self, ngpu, num_conv_layers):
+        super(OptDis, self).__init__()
+        self.ngpu = ngpu
+        self.num_filters = [2**(i+6) for i in range(num_conv_layers-1)]
+        self.num_filters.append(1)
+        self.strides = [2]
+        self.paddings = [1]
+        self.kernelSizes = [4]
+        self.numberChannels = 3 # could be an input
+        self.out_size = []
+        if num_conv_layers == 3:
+            self.strides.extend([2,2])
+            self.paddings.extend([0,0])
+            self.kernelSizes.extend([14,10])
+        if num_conv_layers == 4:
+            self.strides.extend([2,2,2])
+            self.paddings.extend([1,0,0])
+            self.kernelSizes.extend([4,6,6])
+        if num_conv_layers == 5:
+            self.strides.extend([2,2,2,2])
+            self.paddings.extend([1,1,1,0])
+            self.kernelSizes.extend([4,4,4,4])
+        if num_conv_layers == 6:
+            self.strides.extend([2,2,2,2,2])
+            self.paddings.extend([1,1,1,1,1])
+            self.kernelSizes.extend([4,4,4,4,4])
+            
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64 -> output nc = 2**6 x 32 x 32
+            nn.Conv2d(in_channels=self.numberChannels, 
+                      out_channels=self.num_filters[0], 
+                      kernel_size=self.kernelSizes[0], 
+                      stride=self.strides[0], 
+                      padding=self.paddings[0], 
+                      bias=False), # (64-4+2)/2 + 1=32
+            nn.LeakyReLU(0.2, inplace=True))
+        for i in range(1, num_conv_layers):
+            # input is nc=(2**(i+5) x 32 x 32 -> output nc = 2**(i+6) x 32*2**(-i) x 32*2**(-i)
+            self.main.add_module(str(3*i-1)+"): Conv_"+str(i+1), nn.Conv2d(in_channels=self.num_filters[i-1],
+                                                            out_channels=self.num_filters[i],
+                                                            kernel_size=self.kernelSizes[i],
+                                                            stride=self.strides[i],
+                                                            padding=self.paddings[i],
+                                                            bias=False))
+            if i + 1 < num_conv_layers: 
+                self.main.add_module(str(3*i)+"): BatchNorm_" + str(i+1), nn.BatchNorm2d(self.num_filters[i]))
+                self.main.add_module(str(1+3*i)+"): LeakyReLU_" + str(i+1), nn.LeakyReLU(negative_slope=0.2, inplace=True))
+        self.main.add_module(str(3*i)+"): Sigmoid", nn.Sigmoid())
+    
+    def forward(self, input):
+        return self.main(input)
