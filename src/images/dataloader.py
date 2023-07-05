b74375593ba98f5e4b136ae08d7da986b3037c73
Pavel Sulimov <sulpav@yandex.ru>
2023-07-05 18:32:05 +0000

    adding processor of data for RGB

--- src/images/dataloader.py
+++ src/images/dataloader.py
@@ -1,3 +1,5 @@
+from os import listdir
+from os.path import isfile, join
 import torch
 from typing import Optional
 from PIL import Image
@@ -14,13 +16,13 @@ class CadastralImage:
     """
     Holding the image of cadastral map along with it's vector representation
     """
-    def __init__(self, src: str, resolution: Optional[tuple] = None, device: Optional[str] = 'cpu'):
+    def __init__(self, src: str, resolution: Optional[tuple] = None, mult: Optional[int] = 11, device: Optional[str] = 'cpu'):
         self.src = src
-        self._load_transform(resolution, device)
+        self._load_transform(resolution, mult, device)
         
-    def _load_transform(self, resolution: Optional[tuple] = None, device: Optional[str] = 'cpu'):
+    def _load_transform(self, resolution: Optional[tuple] = None, mult: Optional[int] = 11, device: Optional[str] = 'cpu'):
         """
-        Loads and transforms the image into 1-D channel representation (with resize if necessary)
+        Loads and transforms the image into 3-D channel representation (with resize if necessary)
         """
         self.image = Image.open(self.src).convert('L')
         self.vec = transforms.functional.to_tensor(self.image).to(device) # "grayscale" convertion from RGB
@@ -30,11 +32,25 @@ class CadastralImage:
                 raise WrongResolutionException(f"Cannot turn image of size {self.image.size} into {resolution}")
             else:
                 resizer = transforms.Resize(resolution)
+                croper = transforms.CenterCrop(resolution[0] * mult)
+                self.vec = croper.forward(self.vec)
                 self.vec = resizer.forward(self.vec)
         # leaving only first dimension after converting
         self.vec = self.vec.reshape(self.vec.shape[1], self.vec.shape[2])
         # leaving only 0.5 value in case of non-binary element
-        self.vec = torch.where((self.vec == 0) + (self.vec == 1), self.vec, 0.5)
+        # self.vec = torch.where((self.vec == 0) + (self.vec == 1), self.vec, 0.5)
+        
+        # Converting an image to 3-channel RGB (roads-greens-buildings)
+        r = torch.zeros_like(self.vec)
+        r[self.vec == True] = 1
+
+        b = torch.zeros_like(self.vec)
+        b[self.vec == False] = 1
+
+        g = torch.ones_like(self.vec)
+        g = g - r - b
+        
+        self.vec = torch.tensor([r.numpy(), g.numpy(), b.numpy()])
         
     def show_image(self):
         """
@@ -42,4 +58,18 @@ class CadastralImage:
         
         NB! If you want to see the original image (not resized), call `self.image` itself in a command line
         """
-        plt.imshow(self.vec.cpu())
+        plt.imshow(self.vec.cpu().permute(1, 2, 0))  # required for correct display of RGB images
+        
+
+def load_folder(src: str, resolution: Optional[tuple] = None, mult: Optional[int] = 11, device: Optional[str] = 'cpu'):
+    """
+    Loading all the files from given folder
+    """
+    dataset = []
+    onlyfiles = [f for f in listdir(src) if isfile(join(src, f))]
+    for file in onlyfiles:
+        filesrc = src + file
+        imholder = CadastralImage(filesrc, resolution=resolution, mult=mult, device=device)
+        dataset.append((imholder.vec, 1))  # because the label of real image is True (i.e. 1)
+    
+    return dataset
