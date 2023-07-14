878c3f889c5fc273e85a6bb4e5a805c243062965
Furio <neithangurthang@yahoo.it>
2023-07-14 16:37:56 +0000

    experiments on data preparation and fake city generator notebook

--- src/data/PrepareData_RGB.py
+++ src/data/PrepareData_RGB.py
@@ -2,47 +2,64 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 import fiona
+import os
 from shapely.geometry import Polygon
+from shapely.strtree import STRtree
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-src = "../../data/zg_av_lv95.gpkg"
-fiona.listlayers(src)
+# src = "../../data/zg_av_lv95.gpkg"
+# fiona.listlayers(src)
 
-zur = gpd.read_file("../../data/zh_av_lv95.gpkg", layer='lcsf')
-zug = gpd.read_file(src, layer='lcsf')
+# zur = gpd.read_file("../../data/zh_av_lv95.gpkg", layer='lcsf')
+# zug = gpd.read_file(src, layer='lcsf')
 
-probMap = {'edificio':'b', 
-           'altro_rivestimento_duro':'r', 
-           'giardino':'g', 
-           'marciapiede':'r',
-           'campo_prato_pascolo':'g',
-           'altro_humus':'g',
-           'vigna':'g',
-           'ferrovia': 'g',
-           'bosco_fitto': 'g', 
-           'bacino_idrico': 'g', 
-           'spartitraffico':'r', 
-           'corso_acqua':'g',
-           'specchio_acqua':'g', 
-           'strada_sentiero':'r',
-           'pietraia_sabbia':'g',
-           'torbiera': 'g',
-           'altra_coltura_intensiva': 'g',
-           'altro_bosco': 'g',
-           'cava_di_ghiaia_discarica': 'g',
-           'pascolo_boscato_fitto': 'g',
-           'canneti': 'g',
-           'roccia': 'g',
-           'bacino_idrico': 'g',
-           'specchio_acqua': 'g'
+cantons = gpd.read_file("../../data/av_lv95.gpkg", layer='lcsf')
+
+r = (1.0, 0.0, 0.0)
+g = (0.0, 1.0, 0.0)
+b = (0.0, 0.0, 1.0)
+
+probMap = {'edificio':b, 
+           'altro_rivestimento_duro': r, 
+           'giardino':g, 
+           'marciapiede':r,
+           'altro_humus': g,
+           'campo_prato_pascolo':g,
+           'vigna': g,
+           'ferrovia': r,
+           'bosco_fitto': g, 
+           'bacino_idrico': g, 
+           'spartitraffico': r, 
+           'corso_acqua':g,
+           'specchio_acqua':g, 
+           'strada_sentiero':r,
+           'pietraia_sabbia':g,
+           'torbiera': g,
+           'altra_coltura_intensiva': g,
+           'altro_bosco': g,
+           'cava_di_ghiaia_discarica': g,
+           'pascolo_boscato_fitto': g,
+           'canneti': g,
+           'roccia': g,
+           'bacino_idrico': g,
+           'specchio_acqua': g,
+           'ghiacciaio_nevaio': g,
+           'pascolo_boscato_rado': g,
+           'altra_senza_vegetazione': g,
+           'aeroporto': r
           }
 
 def cut_and_print_probs(geometries, step, path):
+    # Construct the STRtree object
+    tree = geometries.sindex
+    
     minX, minY, maxX, maxY = geometries.total_bounds
     repsX, repsY= int((maxX-minX)/step), int((maxY-minY)/step)
+    counter = 0
     for x in range(repsX):#range(1):
         for y in range(repsY): #range(1):
+            print(f'Progress: {y + x * repsY} / {repsX*repsY}')
             box = gpd.GeoSeries([Polygon([(minX + (x+0) * step, minY + (y+0) * step),
                                          (minX + (x+1) * step, minY + (y+0) * step),
                                          (minX + (x+1) * step, minY + (y+1) * step),
@@ -50,15 +67,20 @@ def cut_and_print_probs(geometries, step, path):
                                       ],)
             boxGDF = gpd.GeoDataFrame.from_features(box)
             boxGDF = boxGDF.set_crs('EPSG:2056')
-            res = gpd.sjoin(geometries, boxGDF, how='inner', predicate='intersects')
-            res['col']= res['Genere'].map(probMap)
+            # select with STRtree
+            selection = list(tree.query(boxGDF.geometry)[1])
+            res = geometries.iloc[selection]
+            # Filter for intersecting triangle
+            # res = [g for g in res if g.intersects(boxGDF.geometry)]
+            # AVOIDING SPATIAL JOIN
+            # res = gpd.sjoin(geometries, boxGDF, how='inner', predicate='intersects')
             nObjs = res.shape[0]
             nBld = res[res.Genere == 'edificio'].shape[0]
-            
             # PLOT
             resolution = 72 # in dpi
             plt.ioff() # turns off interactive plotting
             if (nObjs > 100) and (nBld>10): # only interesting plots
+                res['col']= res['Genere'].map(probMap)#.fillna(g)
                 filename = path + 'CadastralX' + str(int(minX+x*step)) + 'Y' + str(int(minY+y*step)) + 'S' + str(step) + ".png" 
                 #larger size, to use the crop in the dataloader
                 res_multiplier = 20
@@ -75,10 +97,14 @@ def cut_and_print_probs(geometries, step, path):
                 res.plot(ax=ax, color=res['col'])
                 plt.savefig(filename)
                 plt.close(fig)
+                print (f'---> saved file nr {counter} with name {filename} in step {y + x * repsY} / {repsX*repsY}')
+                counter += 1
 
     return
 
-pathRGB = '../../../cadastralExportRGB/'
+pathRGB = '../../../cadastralExportRGB/train/'
 
-cut_and_print_probs(zur, 250, pathRGB)
-cut_and_print_probs(zug, 250, path=pathRGB)
+cut_and_print_probs(cantons, 250, pathRGB)
+                  
+print(f'Total files in train folder: {len([entry for entry in os.listdir(pathRGB)])}')
+# cut_and_print_probs(zug, 250, path=pathRGB)
